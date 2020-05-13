5b39df10d043c767431fb346c81709a997b6c41c
Sobira <sobira204@gmail.com>
2020-05-13 15:12:00 +0500

    Fourth ex is done

--- Program.cs
+++ Program.cs
@@ -22,15 +22,25 @@ namespace EF
             foreach(var m in an.Reverse())
             System.Console.Write(m + " ");
 
-            System.Console.WriteLine();
+
+            System.Console.WriteLine("\n");
             int[] array = {1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15};
             int[] resultArr = {array.Where(x => x > 0).Count(), array.Where(x => x < 0).Sum()};
             foreach(var v in resultArr)
             System.Console.WriteLine(v);
+            
 
+            System.Console.WriteLine();
             string[] s = {"Telescopes", "Glasses", "Eyes", "Monocles"};
             foreach(var ss in s.OrderBy(x => x.Length))
             System.Console.WriteLine(ss);
+            
+
+            System.Console.WriteLine();
+            int[] numbers = {1,1,1,2,1,1};
+            var snum = numbers.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key);
+            foreach(var x in snum)
+            System.Console.WriteLine(x);
         }
     }
 }
